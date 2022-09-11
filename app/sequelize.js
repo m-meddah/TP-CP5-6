8e020fe4f8f4363ccc72e6bef1f3d8e6d827c162
m-meddah <meddah.madjid@gmail.com>
2022-09-11 17:37:52 +0200

    comments removed

--- app/sequelize.js
+++ app/sequelize.js
@@ -11,15 +11,4 @@ const sequelize = new Sequelize(process.env.PG_URL,
         }
     });
 
-// async function connect() {
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//         } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//         }
-// }
-
-// connect();
-
 module.exports = sequelize;
